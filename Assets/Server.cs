7ea1a1428e5f2449d178bd8ae3830eb1087c71cf
FatFingersJackson <fatfingersjackson@gmail.com>
2017-08-22 04:39:59 +0300

    Multiplayer

---
+++ Assets/Server.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Rendering;
+
+public class Server : MonoBehaviour {
+
+    bool IsHeadless() { return SystemInfo.graphicsDeviceType == GraphicsDeviceType.Null; }
+
+
+    NetworkClient myClient;
+    public bool isConnected{ get { return _connected; } } 
+    bool _connected { get; set;} 
+
+	// Use this for initialization
+	void Start () {
+
+        #if DEBUG
+
+        SetupServer();
+
+        #else
+
+
+        SetupClient();
+
+        #endif
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void SetupServer()
+    {
+        
+        NetworkServer.Listen(4444);
+    }
+
+    /*-----  --  ---  -- -- -- --*/
+
+    // Create a client and connect to the server port
+    public void SetupClient()
+    {
+        myClient = new NetworkClient();
+        myClient.RegisterHandler(MsgType.Connect, OnConnected);     
+        myClient.Connect("127.0.0.1", 4444);
+    }
+
+    public void OnConnected(NetworkMessage netMsg)
+    {
+        Debug.Log("Connected to server");
+        _connected = true;
+    }
+
+}
---
+++ Assets/TestScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class TestScript : NetworkBehaviour
+{
+
+    bool serverIsConnected { get; set; }
+
+    int counter;
+    [ClientRpc]
+    public void RpcDoMagic(int extra)
+    {
+        Debug.Log("Magic = " + (123 + extra));
+    }
+
+    void Update()
+    {
+       
+            RpcDoMagic(counter);
+
+    }
+}
